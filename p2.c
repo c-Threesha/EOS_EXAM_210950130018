0f6014a638804d19a6f6805d812e82676e3732c5
Trishula <trishachikhale16@gmail.com>
2021-12-14 13:07:15 +0530

    exam

--- p2.c
+++ p2.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
-{
-    char *filename = "dictionary.txt";
+int main(int argc, char const *argv[]){
+
+    unsigned char read_buffer[128];
+    pid_t id;
+    int pfd[2];     //Array of two integers
+    pipe(pfd);      //Pipe is created
+
+    id = fork();         //Child process is created using fork
+    if(0 == id){
+        //Child Process
+        read(pfd[0],read_buffer,128);
+        printf("In Child: %s\n", read_buffer);
+        close(pfd[0]);
+    }else{
+        //Parent Process
+         char *filename = "dictionary.txt";
     FILE *fp = fopen(filename, "r");
 
     if (fp == NULL)
@@ -15,10 +29,8 @@ int main()
     // display it to the output
     char ch;
     while ((ch = fgetc(fp)) != EOF)
-        putchar(ch);
-
-    // close the file
-    fclose(fp);
-
+        write(pfd[1],&ch,15);
+        close(pfd[1]);
+    }
     return 0;
 }
